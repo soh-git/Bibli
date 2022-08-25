54de168e9c20b96e3f0169c5fd5687037f088e93
sm-git <mze.souhaib@gmail.com>
2022-08-25 14:09:12 +0200

    up

--- Javascript-function/string.js
+++ Javascript-function/string.js
@@ -5,6 +5,14 @@ function countWords(str) {
   }
 
 function removePonctution(s) {
-    var punctuationless = s.replace(/[.,\/#!?$%\^\*;:{}<>=\-_`~()]/g,"");
+    var punctuationless = s.replace(/[.,\/#!?§%\^&\*;:{<>}=\-_`~()]/g,"");
 return  punctuationless.replace(/\s{2,}/g," ");
 }
+
+function  NbWordsDistincts(str) {
+    str = removePonctution (str)
+    const arr = str.split(" ").filter(word => word !== '')
+    var nb= new Set( arr)
+    return nb.size
+}
+console.log(NbWordsDistincts(" je ne sais pas. pas du tout mais mais"))
