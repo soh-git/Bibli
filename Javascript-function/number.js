f03b52278f883b46d4079c543d485abd7c9ff8a2
sm-git <mze.souhaib@gmail.com>
2022-09-01 08:37:07 +0200

    util numbers functions

---
+++ Javascript-function/number.js
@@ -0,0 +1,52 @@
+function isInt(value) {
+    return !isNaN(value) && (function(x) { return (x | 0) === x; })(parseFloat(value))
+  }
+  
+  function isFloat(n){
+      return Number(n) === n && n % 1 !== 0;
+  }
+  
+  function isNumeric(value) {
+    return !isNaN(parseFloat(value)) && isFinite(value);
+  }
+  
+  function numericValidator(value) {
+      if (value) {
+          if (! /\D/.test(value)) {
+              return true;
+          } else {
+              return false;
+          }
+      } else {
+          return false;
+      }
+  }
+  
+  //return true if the number is odd
+  function isOdd(num) {
+    return num % 2 != 0;
+  }
+  
+  // return true if the number is even
+  function isEven(num){
+    return num % 2 == 0;
+  }
+  
+  // return true if the number is prime
+  function isPrime(num){
+    if (! num || ! isNumeric(num)) {
+      return false;
+    if(num==2) 
+      return true;
+    for(i=2;i<Math.sqrt(num);i++) {
+      if(num % i==0) 
+        return false; // otherwise it's a prime no.
+    }
+    return true;
+  }
+      
+      
+  function getRandomNumber() {
+      //generates 8 digit random integer as string
+      return Math.floor((Math.random() * 100000000) + 9999999).toString();
+  }
