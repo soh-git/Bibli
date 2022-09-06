6b6c5e81a8a051e54ad43040b77333eb80d9ee70
sm-git <mze.souhaib@gmail.com>
2022-09-06 09:54:52 +0200

    update

--- Javascript-function/array.js
+++ Javascript-function/array.js
@@ -1,36 +1,46 @@
-module.exports ={
- allElementsDistincts:(array) => {
+
+ function allElementsDistincts(array)  {
     let s = new Set();
     array.forEach(el=>{s.add(el)})
     return (s.size == array.length);
-},
-  removeDeplicateEl:(myArray)=> {
+}
+ function  removeDeplicateEl(myArray) {
     return [... new Set(myArray)]
-} ,
- removeFalseEl(myArray) {
+} 
+ function removeFalseEl(myArray) {
     return myArray.filter(Boolean)
-}, 
-
 }
 
 
 
 
+ const TwoArraysAreEquals = (a, b) => a.length === b.length &&a.every((v, i) => v === b[i]);
 
-// export const TwoArraysAreEquals = (a, b) => a.length === b.length &&a.every((v, i) => v === b[i]);
-
-// //   Intersection
-// export function intersection(arr1,arr2) { return arr1.filter(x => arr2.includes(x)) }
-// export function difference(arr1,arr2) { arr1.filter(x => !arr2.includes(x)) }
+//   Intersection
+ function intersection(arr1,arr2) { return arr1.filter(x => arr2.includes(x)) }
+ function difference(arr1,arr2) { arr1.filter(x => !arr2.includes(x)) }
 
     
-// // For a symmetric difference, you can do:   
-// // let differenceSym = arr1.filter(x => !arr2.includes(x)).concat(arr2.filter(x => !arr1.includes(x)));
-// export function shuffle(myArray){ return myArray.sort(() => Math.random() - 0.5)}
+;
+ function shuffle(myArray){ return myArray.sort(() => Math.random() - 0.5)}
 
-// export function cloneArray(myArray){
-//     return myArray.map(a => Array.isArray(a) ? cloneArray(item) : a);
+ function cloneArray(myArray){
+    return myArray.map(a => Array.isArray(a) ? cloneArray(item) : a);
+}
+
+
+// module.s ={
+//  allElementsDistincts:(array) => {
+//     let s = new Set();
+//     array.forEach(el=>{s.add(el)})
+//     return (s.size == array.length);
+// },
+//   removeDeplicateEl:(myArray)=> {
+//     return [... new Set(myArray)]
+// } ,
+//  removeFalseEl(myArray) {
+//     return myArray.filter(Boolean)
+// }, 
+
 // }
 
-
-// // helpers https://medium.com/swlh/array-helper-methods-in-es6-28fc5e5a5dc9
