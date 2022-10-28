823bb32f730ed425aa8491903522e4efb0123d85
soh-git <86286924+soh-git@users.noreply.github.com>
2022-10-28 18:28:35 +0200

    Update geometry.js

--- Javascript-function/geometry.js
+++ Javascript-function/geometry.js
@@ -1,6 +1,6 @@
 function getDistanceBetweenTwoPoints(x1, y1, x2, y2){
-    let y = x2 - x1;
-    let x = y2 - y1;
+    let x = x2 - x1;
+    let y = y2 - y1;
     
     return Math.sqrt(x * x + y * y);
 }
